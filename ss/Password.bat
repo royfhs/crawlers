f0398e64ccd76e510ba0f095f506bb2eea13d8f5
roy <royfhs@163.com>
2016-01-31 01:07:26 +0800

    For www.ishadowsocks.com. To crawling the password for free shadowsocks server.

---
+++ ss/Password.bat
@@ -0,0 +1,3 @@
+@echo off
+python ishadowsocks.py
+pause
---
+++ ss/ishadowsocks.py
@@ -0,0 +1,13 @@
+import urllib, re
+
+print urllib.urlopen('http://www.ishadowsocks.com/').getcode()
+
+urllib.urlretrieve('http://www.ishadowsocks.com/',filename='ss.html')
+
+ss = open("ss.html","r+").read()
+m = re.search(r'us1\.iss\.tf',ss, re.M).span()[0]
+if m:
+	print "Password:",
+	print ss[m+68:m+76]
+else:
+	print "error"
